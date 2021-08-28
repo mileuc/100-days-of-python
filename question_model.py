c6475551e900ff445a54164f54c9560558b07d2f
Michael Le <72768694+mileuc@users.noreply.github.com>
2021-08-27 18:07:53 -0600

    Add files via upload

---
+++ question_model.py
@@ -0,0 +1,6 @@
+# TODO: 1. Create a model for a question in the quiz
+class Question:
+    # initializing attributes
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
