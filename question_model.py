bad54dcd78fc1a71f070bb2bf1c2627beed822da
Michael Le <72768694+mileuc@users.noreply.github.com>
2021-08-27 18:59:10 -0600

    Delete question_model.py

--- question_model.py
+++
@@ -1,6 +0,0 @@
-# TODO: 1. Create a model for a question in the quiz
-class Question:
-    # initializing attributes
-    def __init__(self, text, answer):
-        self.text = text
-        self.answer = answer
