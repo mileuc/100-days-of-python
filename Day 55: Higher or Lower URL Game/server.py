28d08fc74495f27588bd95e0de10c5c2ba4e560e
Michael Le <72768694+mileuc@users.noreply.github.com>
2021-08-27 21:48:32 -0600

    Add files via upload

---
+++ Day 55: Higher or Lower URL Game/server.py
@@ -0,0 +1,29 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+random_number = random.randint(1, 9)
+
+
+@app.route('/')
+def home_page():
+    return "<h1>Guess a number between 0 and 9</h1>" \
+           "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'>"
+
+
+@app.route('/<int:guessed_number>')
+def check_guess(guessed_number):
+    if guessed_number < random_number:
+        return '<h1 style="color: red">Too low, try again!</h1>' \
+               '<img src="https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif">'
+    elif guessed_number > random_number:
+        return '<h1 style="color: purple">Too high, try again!</h1>' \
+               '<img src="https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif">'
+    else:
+        return '<h1 style="color: green">You found me!</h1>' \
+               '<img src="https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif">'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
